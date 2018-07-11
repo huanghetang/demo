02e05339ce4f11b05665b9ffbd3fd27f32b02194
huanghetang <1278699240@qq.com>
2018-07-11 22:12:12 +0800

    7/11 springboot集成测试

--- springboot_demo/src/test/java/WebTest.java
+++ springboot_demo/src/test/java/WebTest.java
@@ -1,7 +1,29 @@
+import com.leyou.Application;
+import com.leyou.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
 /**
  * @author zhoumo
  * @datetime 2018/7/11 20:55
- * @desc
+ * @desc springboot集成junit测试
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+//springboot测试注解,指定@SpringBootConfiguration 即springboot的配置类,此处填写的是SpringBoot应用入口类
+@SpringBootTest(classes = Application.class)
 public class WebTest {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void test1(){
+        List list = userService.findUserByExample("幂");
+        list.forEach(i-> System.out.println("i = " + i));
+    }
 }
